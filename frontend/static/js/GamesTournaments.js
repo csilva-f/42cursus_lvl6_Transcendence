871e1b6a0762069dee7b1b7314543db9f4a45e95
souzitaaaa <diogo.nogueirasousa123@gmail.com>
2024-11-13 00:47:23 +0000

    GameCard Load when fetched data and css improvements

--- frontend/static/js/GamesTournaments.js
+++ frontend/static/js/GamesTournaments.js
@@ -1,5 +1,12 @@
 
 async function fetchGames() {
+    const userLang = localStorage.getItem("language") || "en";
+    const langData = await getLanguageData(userLang);
+    const reloadIcon = document.getElementById('reloadIcon');
+    reloadIcon.classList.add('rotate');
+    setTimeout(() => {
+        reloadIcon.classList.remove('rotate');
+    }, 250);
     fetch('/templates/Components/GameCard.html').then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
@@ -14,21 +21,36 @@ async function fetchGames() {
                 const divElement = document.getElementById("gamesContent");
                 gamesContent.innerHTML = "";
                 res.games.forEach(element => {
-                    const newDiv = document.createElement('div');
-                    newDiv.innerHTML = data;
-                    //const enterBtn = newDiv.querySelector('#enterLi');
-                    //enterBtn.setAttribute('data-id', element.id);
-                    //const title = newDiv.querySelector('#game-title');
-                    //title.textContent = element.id;
-                    divElement.appendChild(newDiv);
+                    const newCard = document.createElement('div');
+                    newCard.innerHTML = data;
+                    const enterBtn = newCard.querySelector('#enterLi');
+                    const user1Level = newCard.querySelector('#user1Level');
+                    const user1Nick = newCard.querySelector('#user1Nick');
+                    const user2Img = newCard.querySelector('#user2Img');
+                    const user2LvlLabel = newCard.querySelector('#user2LvlLabel');
+                    const user2Level = newCard.querySelector('#user2Level');
+                    const user2Nick = newCard.querySelector('#user2Nick');
+                    enterBtn.setAttribute('data-id', element.id);
+                    user1Level.textContent = element.user1;
+                    user1Nick.textContent = "{Nickname}";
+                    if (element.user2 == null) {
+                        user2Img.src = "/static/img/pfpWaiting.jpg";
+                        user2LvlLabel.style.display = 'none';
+                        user2Nick.setAttribute('data-i18n', "waiting");
+                    } else {
+                        user2Level.textContent = element.user2;
+                        user2Nick.textContent = "{Nickname2}";
+                    }
+                    divElement.appendChild(newCard);
                 });
+                updateContent(langData);
             },
             error: function (xhr, status, error) {
                 console.log("Error:", error);
             },
         });
     })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 }
