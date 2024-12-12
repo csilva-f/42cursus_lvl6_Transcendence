71f9464c8be960d07218c1f0551aad5492e6141e
souzitaaaa <diogo.nogueirasousa123@gmail.com>
2024-12-12 12:49:57 +0000

    Local Pong Game almost done, and bug fixing

--- frontend/static/js/Game/Game.js
+++ frontend/static/js/Game/Game.js
@@ -10,11 +10,40 @@ window.addEventListener('keyup', function (e) {
     keyPressed[e.keyCode] = false;
 })
 
+function respawnBall(canvas, objects) {
+    if (objects[0].ballVelocityX > 0) {
+        objects[0].ballX = canvas.width / 2;
+        objects[0].ballY = (Math.random() * (canvas.height - 200)) + 100;
+    }
+    if (objects[0].ballVelocityX < 0) {
+        objects[0].ballX = canvas.width / 2;
+        objects[0].ballY = (Math.random() * (canvas.height - 200)) + 100;
+    }
+    objects[0].ballVelocityX *= -1;
+    objects[0].ballVelocityY *= -1;
+}
+
+function incScore(canvas, objects) {
+    if (objects[0].ballX <= -objects[0].ballRadius){
+        objects[2].paddleScore += 1;
+        document.getElementById("playerRightScore").innerHTML = objects[2].paddleScore;
+        respawnBall(canvas, objects);
+    }
+    if (objects[0].ballX >= canvas.width + objects[0].ballRadius){
+        objects[1].paddleScore += 1;
+        document.getElementById("playerLeftScore").innerHTML = objects[1].paddleScore;
+        respawnBall(canvas, objects);
+    }
+}
+
 function gameUpdate(canvas, objects) {
     objects.forEach(element => {
         element.update();
         element.colissionEdge(canvas);
+        if (element instanceof Paddle)
+            element.colissionBall(objects[0], element);
     });
+    incScore(canvas, objects);
 }
 
 function gameDraw(ctx, objects) {
@@ -28,8 +57,8 @@ function gameLoop(canvas, ctx, objects) {
         //otimizar isto e depois tambem dar update nas posicoes dos paddles e da bola
         const rootStyle = getComputedStyle(document.documentElement);
         const remToPixels = parseFloat(rootStyle.fontSize);
-        canvas.width = window.innerWidth - (2 * remToPixels);
-        canvas.height = window.innerHeight - (8 * remToPixels);
+        canvas.width = window.innerWidth - (50 * remToPixels);
+        canvas.height = window.innerHeight - (20 * remToPixels);
     }
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     window.requestAnimationFrame(() => gameLoop(canvas, ctx, objects));
@@ -45,11 +74,11 @@ function initGame() {
 
     const rootStyle = getComputedStyle(document.documentElement);
     const remToPixels = parseFloat(rootStyle.fontSize);
-
+    
     //TODO Entender pk que a width esta errada
-    canvas.width = window.innerWidth - (2 * remToPixels);
-    canvas.height = window.innerHeight - (8 * remToPixels);
-
+    canvas.width = window.innerWidth - (50 * remToPixels);
+    canvas.height = window.innerHeight - (20 * remToPixels);
+    
     const objects = [
         new Ball(canvas.width / 2, canvas.height / 2, 10, 10, 15),
         new Paddle(1, 20, 150, "#AC3B61", 30, canvas.height / 2, 10),
@@ -59,5 +88,8 @@ function initGame() {
     //* objects[1]: PaddleLeft
     //* objects[2]: PaddleRight
 
+    document.getElementById("playerLeftScore").innerHTML = objects[1].paddleScore;
+    document.getElementById("playerRightScore").innerHTML = objects[2].paddleScore;
+    
     gameLoop(canvas, ctx, objects);
 }
--- frontend/static/js/Game/classes.js
+++ frontend/static/js/Game/classes.js
@@ -1,5 +1,5 @@
 class Ball {
-    constructor(ballX, ballY, ballVelocityX, ballVelocityY, ballRadius) {   
+    constructor(ballX, ballY, ballVelocityX, ballVelocityY, ballRadius) {
         this.ballX = ballX;
         this.ballY = ballY;
         this.ballRadius = ballRadius;
@@ -16,28 +16,33 @@ class Ball {
         ctx.arc(this.ballX, this.ballY, this.ballRadius, 0, Math.PI * 2);
         ctx.fill()
     }
-    colissionEdge(canvas){
+    colissionEdge(canvas) {
         if (this.ballY + this.ballRadius >= canvas.height)
             this.ballVelocityY *= -1;
         if (this.ballY - this.ballRadius <= 0)
             this.ballVelocityY *= -1;
-        if (this.ballX + this.ballRadius >= canvas.width)
-            this.ballVelocityX  *= -1;
-        if (this.ballX - this.ballRadius <= 0)
-            this.ballVelocityX *= -1;
+        // if (this.ballX + this.ballRadius >= canvas.width)
+        //     this.ballVelocityX *= -1;
+        // if (this.ballX - this.ballRadius <= 0)
+        //     this.ballVelocityX *= -1;
     }
 }
 
 class Paddle {
     constructor(paddleSide, paddleWidth, paddleHeight, paddleColor, paddleX, paddleY, paddleVelocityY) {
-        this.paddleSide = paddleSide; 
+        this.paddleSide = paddleSide;
         this.paddleWidth = paddleWidth;
         this.paddleHeight = paddleHeight;
+        this.paddleScore = 0;
         this.paddleColor = paddleColor;
         this.paddleX = paddleX;
         this.paddleY = paddleY;
         this.paddleVelocityY = paddleVelocityY;
     }
+    getHalfWidth() { return this.paddleWidth / 2; }
+    getHalfHeight() { return this.paddleHeight / 2; }
+    getCenterWidth() { return this.paddleX + this.getHalfWidth(); }
+    getCenterHeight() { return this.paddleY + this.getHalfHeight(); }
     update() {
         if (this.paddleSide == 2) {
             if (keyPressed[KEY_ARROWUP])
@@ -55,10 +60,17 @@ class Paddle {
         ctx.fillStyle = this.paddleColor;
         ctx.fillRect(this.paddleX, this.paddleY, this.paddleWidth, this.paddleHeight);
     }
-    colissionEdge(canvas){
+    colissionEdge(canvas) {
         if (this.paddleY + this.paddleHeight >= canvas.height)
             this.paddleY = canvas.height - this.paddleHeight;
         if (this.paddleY <= 0)
             this.paddleY = 0;
+    }
+    colissionBall(ball) {
+        let dX = Math.abs(ball.ballX - this.getCenterWidth());
+        let dY = Math.abs(ball.ballY - this.getCenterHeight());
+
+        if (dX <= (ball.ballRadius + this.getHalfWidth()) && dY <= (ball.ballRadius + this.getHalfHeight()))
+            ball.ballVelocityX *= -1;
     }
 }
