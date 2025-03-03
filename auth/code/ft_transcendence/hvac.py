571a6ac7b2b6a8fd9eeef3d25323a1e845fa0f60
Bruno Camarinha <bcamarinha@MacBook-Pro-de-Bruno.local>
2025-03-03 19:25:54 +0000

    vault working with env variables. moving to docker secrets.

--- auth/code/ft_transcendence/hvac.py
+++ auth/code/ft_transcendence/hvac.py
@@ -1,8 +1,17 @@
 import os
 import hvac
 
+def read_docker_secret(secret_name):
+    secret_path = os.getenv('VAULT_ROOT_TOKEN_FILE',f'/run/secrets/{secret_name}.txt')
+    try:
+        with open(secret_path, 'r') as secret_file:
+            return secret_file.read().strip()
+    except FileNotFoundError:
+        return None
+
+
 def get_database_credentials():
-    client = hvac.Client(url=os.getenv('VAULT_ADDR'), token=os.getenv('VAULT_TOKEN'))
+    client = hvac.Client(url=os.getenv('VAULT_ADDR'), token=read_docker_secret('VAULT_ROOT_TOKEN'))
     try:
         response = client.secrets.database.generate_credentials('role-auth-db')
         return response['data']['username'], response['data']['password']
--- vault/tools/init.sh
+++ vault/tools/init.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+POSTGRES_USER=$(cat "$POSTGRES_USER_FILE")
+POSTGRES_PASSWORD=$(cat "$POSTGRES_PASSWORD_FILE")
+
 cat <<EOF > /vault/vault.hcl
 storage "postgresql" {
     connection_url = "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_DB}:5432/${POSTGRES_DB}?sslmode=disable"
@@ -21,53 +24,54 @@ sleep 5
 
 # Set the VAULT_ADDR environment variable to use HTTP
 export VAULT_ADDR="http://0.0.0.0:8200"
-
-# Initialize Vault if it is not already initialized
-if ! vault status | grep -q 'Initialized'; then
+if ! vault status | grep -q 'Initialized        true'; then
   echo "Initializing Vault..."
-  vault operator init -key-shares=1 -key-threshold=1 > /tmp/init_output.txt
+  vault operator init -key-shares=1 -key-threshold=1 > /vault/file/init_output.txt
   # Extract the unseal key and root token
-  UNSEAL_KEY=$(grep 'Unseal Key 1:' /tmp/init_output.txt | awk '{print $NF}')
-  ROOT_TOKEN=$(grep 'Initial Root Token:' /tmp/init_output.txt | awk '{print $NF}')
+  UNSEAL_KEY=$(grep 'Unseal Key 1:' /vault/file/init_output.txt | awk '{print $NF}')
+  ROOT_TOKEN=$(grep 'Initial Root Token:' /vault/file/init_output.txt | awk '{print $NF}')
 
   # Unseal Vault using the unseal key
   echo "Unsealing Vault..."
   vault operator unseal "$UNSEAL_KEY"
 
   # Store the root token in an environment variable or a file
-  echo "Root Token: $ROOT_TOKEN"
-
-else
-  echo "Vault is already initialized."
-    ROOT_TOKEN=myroot
+  echo "Root Token: $ROOT_TOKEN" > /vault/file/root_token.txt
+  echo $ROOT_TOKEN > /vault/secrets/VAULT_ROOT_TOKEN.txt
   vault login $ROOT_TOKEN
-
-  # Enable the database secrets engine
-  #
-    if ! vault secrets list | grep -q "database/"; then
-        echo "Enabling the database secrets engine..."
-        vault secrets enable database
-
-        # Configure the PostgreSQL database connection
-        vault write database/config/config-auth-db \
-            plugin_name=postgresql-database-plugin \
-            allowed_roles="role-auth-db" \
-            connection_url="postgresql://{{username}}:{{password}}@auth-db:5432/auth-db?sslmode=disable" \
-            username="eva" \
-            password="gina"  # Replace with actual credentials
-
-        # Create the role for Django
-        vault write database/roles/role-auth-db \
-            db_name=config-auth-db \
-            creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}'; GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
-            default_ttl="1h" \
-            max_ttl="24h"
-
-        vault write -f database/rotate-root/config-auth-db
-    else
-        echo "Database secrets engine is already enabled."
-    fi
+else
+  echo "Vault is already initialized"
+  UNSEAL_KEY=$(grep 'Unseal Key 1:' /vault/file/init_output.txt | awk '{print $NF}')
+  echo "Unsealing Vault..."
+  vault operator unseal "$UNSEAL_KEY"
+  ROOT_TOKEN=$(grep 'Root Token:' /vault/file/root_token.txt | awk '{print $NF}')
+  echo $ROOT_TOKEN > /vault/secrets/VAULT_ROOT_TOKEN.txt
+  vault login $ROOT_TOKEN
 fi
 
-# Keep the container running
+
+if ! vault secrets list | grep -q "database/"; then
+    echo "Enabling the database secrets engine..."
+    vault secrets enable database
+
+    # Configure the PostgreSQL database connection
+    vault write database/config/config-auth-db \
+        plugin_name=postgresql-database-plugin \
+        allowed_roles="role-auth-db" \
+        connection_url="postgresql://{{username}}:{{password}}@auth-db:5432/auth-db?sslmode=disable" \
+        username="eva" \
+        password="gina"  # Replace with actual credentials
+
+    # Create the role for Django
+    vault write database/roles/role-auth-db \
+        db_name=config-auth-db \
+        creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}'; GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
+        default_ttl="1h" \
+        max_ttl="24h"
+
+    vault write -f database/rotate-root/config-auth-db
+else
+    echo "Database secrets engine is already enabled."
+fi
+# Initialize Vault if it is not already initialized
 tail -f /dev/null
