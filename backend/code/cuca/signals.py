f25160a3b739772f0fe30425cb4b5cee152b721d
Bruno Camarinha <bcamarinha@MacBook-Pro-de-Bruno.local>
2025-03-11 18:59:49 +0000

    add signals file in order to fill database static tables

--- backend/code/cuca/signals.py
+++ backend/code/cuca/signals.py
@@ -2,12 +2,30 @@
 
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-from .models import tauxStatus
+from .models import tauxStatus, tauxGender, tauxPhase, tauxFriendshipStatus, tUserExtension
 
 @receiver(post_migrate)
 def load_initial_data(sender, **kwargs):
-    if sender.name == 'yourapp':  # Ensure this runs only for your app
+    if sender.name == 'cuca':  # Ensure this runs only for your app
         if not tauxStatus.objects.exists():
             tauxStatus.objects.create(status="Searching")
             tauxStatus.objects.create(status="Happening")
-            tauxStatus.objects.create(status="Finished")
+            tauxStatus.objects.create(status="Finished") # Ensure this runs only for your app
+
+        if not tauxGender.objects.exists():
+            tauxGender.objects.create(label="Male")
+            tauxGender.objects.create(label="Female")
+            tauxGender.objects.create(label="Other")
+
+        if not tauxPhase.objects.exists():
+            tauxPhase.objects.create(label="Quarter final")
+            tauxPhase.objects.create(label="Semi final")
+            tauxPhase.objects.create(label="Final")
+
+        if not tauxFriendshipStatus.objects.exists():
+            tauxFriendshipStatus.objects.create(label="RequestSent")
+            tauxFriendshipStatus.objects.create(label="Friends")
+            tauxFriendshipStatus.objects.create(label="NotFriends")
+
+        if not tUserExtension.objects.exists():
+            tUserExtension.objects.create(user=-1, nick="Guest")
