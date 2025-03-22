b0bde93840cd8dbf49ba220ae63f25965898dbf4
Bruno Manuel De Sousa Camarinha <bde-sous@c1r4s7.42porto.com>
2025-03-22 11:59:02 +0000

    fix container ttl

--- email/code/cuca_db_engine/base.py
+++ email/code/cuca_db_engine/base.py
@@ -1,7 +1,11 @@
 from django.db.backends.postgresql.base import DatabaseWrapper as PostgresDatabaseWrapper
-from .hvac import get_database_credentials
+from .hvac import VaultDatabaseCredentials
 
 class DatabaseWrapper(PostgresDatabaseWrapper):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.vault_creds = VaultDatabaseCredentials()  # Instantiate the VaultDatabaseCredentials class
+
     def get_connection_params(self):
         # Fetch credentials from your vault
         credentials = self.get_credentials_from_vault()
@@ -11,10 +15,16 @@ class DatabaseWrapper(PostgresDatabaseWrapper):
         return params
 
     def get_credentials_from_vault(self):
-        DB_USERNAME, DB_PASSWORD = get_database_credentials()
-        print(f"DB_USERNAME: {DB_USERNAME}")
-        print(f"DB_PASSWORD: {DB_PASSWORD}")
-        return {
-            'username': DB_USERNAME,
-            'password': DB_PASSWORD,
-        }
+        # Get database credentials from Vault
+        credentials = self.vault_creds.get_database_credentials()
+        if credentials is None:
+            raise Exception("Failed to retrieve database credentials from Vault.")
+
+        #print(f"DB_USERNAME: {credentials['username']}")
+        #print(f"DB_PASSWORD: {credentials['password']}")
+
+        # Check if the lease is expiring soon and renew if necessary
+        if self.vault_creds.is_lease_expiring_soon():
+            self.vault_creds.renew_lease()
+
+        return credentials
