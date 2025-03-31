b4faa01104ae88b43bac4882fdddba22014ce0aa
Jennifer De Araujo Oliveira <jede-ara@c1r3s1.42porto.com>
2025-03-31 18:57:07 +0100

    nothing

--- frontend/static/js/Register/MFA.js
+++ frontend/static/js/Register/MFA.js
@@ -6,7 +6,6 @@ function handleOTPInput(field) {
   if (field.value && nextField) nextField.focus();
 
   field.addEventListener("keydown", function (e) {
-
     if (e.key === "Backspace" && !field.value && prevField) prevField.focus();
   });
 
@@ -27,35 +26,6 @@ function handleOTPInput(field) {
     if (lastField) lastField.focus();
   });
 }
-//!FUNCTION DUPLICATED CHECK WHICH ONE IS THE REAL ONE
-// function handleOTPInput(field) {
-//   const currentId = parseInt(field.id.replace("otp", ""));
-//   const nextField = document.getElementById(`otp${currentId + 1}`);
-//   const prevField = document.getElementById(`otp${currentId - 1}`);
-
-//   if (field.value && nextField) nextField.focus();
-
-//   field.addEventListener("keydown", function (e) {
-//     if (e.key === "Backspace" && !field.value && prevField) prevField.focus();
-//   });
-
-//   field.addEventListener("paste", function (e) {
-//     e.preventDefault();
-//     const pasteData = e.clipboardData.getData("text").split("");
-//     let currentId = parseInt(field.id.replace("otp", ""));
-
-//     pasteData.forEach((char) => {
-//       const targetField = document.getElementById(`otp${currentId}`);
-//       if (targetField) {
-//         targetField.value = char;
-//         currentId++;
-//       }
-//     });
-
-//     const lastField = document.getElementById(`otp${currentId - 1}`);
-//     if (lastField) lastField.focus();
-//   });
-// }
 
 function clearOTPFields() {
   for (let i = 1; i <= 6; i++) {
